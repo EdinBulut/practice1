0246aea556bc747c93d9e32e85e57c2f0b6d6448
EdoB <edin.bulut@hotmail.com>
2022-02-19 22:39:27 +0100

    content edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'practicing-1';
+export class AppComponent implements AfterContentInit {
+  canShow = false
+
+  ngAfterContentInit(): void {
+      this.canShow = true
+  }
 }
