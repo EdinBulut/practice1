87cbc0d79a845f4835f2679d99c091e8a9510e62
EdoB <edin.bulut@hotmail.com>
2022-02-19 19:16:08 +0100

    styles formating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'home', component: HomeComponent },
   {path:'products', component: ProductsComponent },
   {path:'about-us', component: AboutUsComponent },
