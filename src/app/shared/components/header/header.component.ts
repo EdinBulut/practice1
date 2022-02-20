780314be3006bd0135d53c9c1d91553e6e317dd9
unknown <sekiseki85@gmail.com>
2022-02-20 19:16:00 +0100

    header footer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -11,28 +11,13 @@ import { HostListener } from '@angular/core';
 export class HeaderComponent implements OnInit {
  
   isScrolled = false;
-
- 
- 
-  constructor(private router: Router,) { 
-    
-  }
+  constructor(private router: Router,) { }
   @HostListener("window:scroll")
   scrollEvent() {
       window.pageYOffset >= 50 ? (this.isScrolled = true) : (this.isScrolled = false);
   }
   
   ngOnInit(): void {
-    console.log(this.isScrolled,"hshshshshshsh");
   }
-  // onWindowScroll() {
-  //   let element = document.querySelector('.nav-container') as HTMLElement;
-  //   if (window.pageYOffset > element.clientHeight) {
-  //     element.classList.add('navbar-inverse');
-  //     console.log("navigacija 111111111")
-  //   } else {
-  //     element.classList.remove('navbar-inverse')
-  //     console.log("navigacija 2");
-  //   }
-  // }
+
 }
