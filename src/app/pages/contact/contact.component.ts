2377890b8cad6d96d010d3c9ff14868bea88b5c6
EdoB <edin.bulut@hotmail.com>
2022-02-26 18:25:36 +0100

    leaflet map added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from 'src/app/shared/services/language.service';
+import { icon, latLng, marker, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +9,8 @@ import { LanguageService } from 'src/app/shared/services/language.service';
 })
 export class ContactComponent implements OnInit {
   language = 'BOS'
+  options: any
+  layers: any
   
   constructor(private languageService: LanguageService) { }
   
@@ -20,6 +23,34 @@ export class ContactComponent implements OnInit {
 
     this.languageService.getLanguage()
     .subscribe(data => this.language = data)
+
+    this.options = {
+      layers: [
+        tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWRpbmJ1bHV0IiwiYSI6ImNrdGl0c282NTE1Z2oybmp5OXdpd3RjNG0ifQ.IzQ1BUhCYwlohdnR1eutGQ', {
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token'
+        })
+      ],
+      zoom: 7,
+      center: latLng(44.982999, 16.698572)
+    };
+
+      this.layers = [
+
+        marker([44.982999, 16.698572], 
+          {
+          icon: icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 41],
+            iconUrl: '../../../assets/images/contact/marker-icon.png',
+            shadowUrl: '../../../assets/images/contact/marker-shadow.png'
+          })
+        }
+        )
+      ]
   }
 
 }
