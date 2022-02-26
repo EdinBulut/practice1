d864a0390be8f034c14eed92d9754738f51f1554
EdoB <edin.bulut@hotmail.com>
2022-02-26 16:31:55 +0100

    x

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -12,24 +12,29 @@ import { LanguageService } from '../../services/language.service';
 export class HeaderComponent implements OnInit {
   mobile = false;
   openMenu = false;
- 
+
   isScrolled = false;
   isTransparent = false;
   language = 'BOS'
   constructor(
-    public router: Router, 
+    public router: Router,
     private languageService: LanguageService,
     private route: ActivatedRoute
-    ) { }
+  ) { }
   @HostListener("window:scroll")
   scrollEvent() {
-      window.pageYOffset >= 60 ? (this.isScrolled = true) : (this.isScrolled = false);
+    window.pageYOffset >= 60 ? (this.isScrolled = true) : (this.isScrolled = false);
   }
   @HostListener("window:resize", [])
   onResize() {
-    (window.innerWidth <= 992) ? this.mobile = true : this.mobile = false
+    if (window.innerWidth <= 992) {
+      this.mobile = true
+    } else {
+      this.mobile = false
+      this.openMenu = false
+    }
   }
-  
+
   ngOnInit(): void {
     if (localStorage.getItem('language')) {
       this.language = localStorage.getItem('language') as string
